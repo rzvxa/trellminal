66dd72a6b7ef841ca605eb326d5d6b7fae3efb86
rzvxa <rzvxa@protonmail.com>
2023-11-10 02:52:11 +0330

    Trying potential error in deserializing users

--- src/api/members.rs
+++ src/api/members.rs
@@ -17,7 +17,7 @@ impl Members for Api {
     async fn members_me(&self) -> Result<User, Box<dyn Error>> {
         let fetch_user_url = format!("{}/?key={}&token={}", URL_ME, self.key, self.token);
         let body = reqwest::get(fetch_user_url).await?.text().await?;
-        let user: User = serde_json::from_str(body.as_str()).unwrap();
-        return Ok(user);
+        let user = serde_json::from_str(body.as_str())?;
+        Ok(user)
     }
 }
