786cb08183898c8d7c1b529057de3b4fe4d68fd9
rzvxa <rzvxa@protonmail.com>
2023-11-14 01:16:12 +0330

    Added layouting utilities

---
+++ src/ui/misc/layout.rs
@@ -0,0 +1,24 @@
+use tui::layout::Rect;
+
+pub fn center_rect(rect: Rect, width: u16) -> Rect {
+    if rect.width <= width {
+        rect.clone()
+    } else {
+        let free_space = rect.width - width;
+        Rect::new(rect.x + free_space / 2, rect.y, width, rect.height)
+    }
+}
+
+pub fn center_rect_with_margin(rect: Rect, width: u16, margin: u16) -> Rect {
+    rect_with_margin(center_rect(rect, width), margin)
+}
+
+pub fn rect_with_margin(rect: Rect, margin: u16) -> Rect {
+    let margin2 = margin * 2;
+    Rect::new(
+        rect.x + margin,
+        rect.y + margin,
+        rect.width - margin2,
+        rect.height - margin2,
+    )
+}
